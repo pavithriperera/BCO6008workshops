82ba69c47223d0bfbe421d8c5a7a2f0644a41d04
Pavithri Perera <pavithri.kuruwitagedona@live.vu.edu.au>
2021-08-04 17:38:02 +1000

    loding library

---
+++ WorkShop1.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse") #installing packages (Do ONCE!)
+install.packages("tidymodels")
+install.packages("skimr")
+install.packages("janitor")
+
+library(tidyverse)
+
+
+
+
+olympics<-read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
+
